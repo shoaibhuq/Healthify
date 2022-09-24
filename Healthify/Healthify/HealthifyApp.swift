9a051f1f53d75806620fbdfe8698147881719520
Shoaib Huq <slimsho123@gmail.com>
2022-09-24 02:31:19 -0500

    added repo

---
+++ Healthify/Healthify/HealthifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthifyApp.swift
+//  Healthify
+//
+//  Created by Shoaib Huq on 9/24/22.
+//
+
+import SwiftUI
+
+@main
+struct HealthifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
